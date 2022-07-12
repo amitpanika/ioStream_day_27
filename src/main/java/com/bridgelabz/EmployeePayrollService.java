c079a823d6694c62d1b13230ad200a20bbb2f034
amit <amitpanika97@gmail.com>
2022-07-12 16:04:15 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -0,0 +1,37 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+    /*
+     *    Creating arrayList of EmployeePayrollData type: EmployeePayrollList
+     */
+    private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();;
+     /*
+      *
+      */
+    public static void main(String[] args) {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeepayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+    }
+
+    private void writeEmployeePayrollData() {
+        System.out.println("\nEmployee Payroll is\n" + employeePayrollList);
+    }
+
+    private void readEmployeepayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee ID");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Employee Name");
+        String Name = consoleInputReader.nextLine();
+        System.out.println("Enter Employee salary");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id, Name, salary));
+
+    }
+
+}
