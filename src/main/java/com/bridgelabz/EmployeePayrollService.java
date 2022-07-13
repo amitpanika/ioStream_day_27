4be8e84c9c61a86b34dad681d30415f4caeb79e1
amit <amitpanika97@gmail.com>
2022-07-13 23:26:27 +0530

    Test for file Operation

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
+    public enum IOService {
+        CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+    }
     /*
      *    Creating arrayList of EmployeePayrollData type: EmployeePayrollList
      */
@@ -20,14 +23,16 @@ public class EmployeePayrollService {
     }
 
     private void writeEmployeePayrollData() {
+
         System.out.println("\nEmployee Payroll is\n" + employeePayrollList);
     }
 
     private void readEmployeepayrollData(Scanner consoleInputReader) {
-        System.out.println("Enter Employee ID");
-        int id = consoleInputReader.nextInt();
+
         System.out.println("Enter Employee Name");
         String Name = consoleInputReader.nextLine();
+        System.out.println("Enter Employee ID");
+        int id = consoleInputReader.nextInt();
         System.out.println("Enter Employee salary");
         double salary = consoleInputReader.nextDouble();
         employeePayrollList.add(new EmployeePayrollData(id, Name, salary));
